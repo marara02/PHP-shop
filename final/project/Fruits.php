be5938b39a9efc53c70e8c7610157fac16ee121e
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-13 14:27:34 +0600

    Update Fruits.php

--- final/project/Fruits.php
+++ final/project/Fruits.php
@@ -4,9 +4,9 @@
 <head>
     <title>onlineshop.kz</title>
     <!--<link rel="stylesheet" type="text/css" href="CSS.css">-->
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="CSS/main.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:600,900" rel="stylesheet">
-    <link rel="shortcut icon" href="/shop.png" type="image/png">
+    <link rel="shortcut icon" href="shop.png" type="image/png">
     <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Six+Caps&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Orbitron&display=swap" rel="stylesheet">
     <link rel = "icon" href = "shop.png" type = "image/x-icon">
-    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="CSS/header.css">
     <script type="text/javascript" src="functions.js"></script>
     <style>
         .wrapper1 {
@@ -56,19 +56,19 @@
         <button class="home" onclick="homeFunction()"><img src="shop.png" class="animation" align="left" width="25"><p style="font-size:185%"><a href ="main.php">Home</a></p></button>
         <div class="dropdown">
             <div id = "list">
-                <button class="dropbtn"><img class="img" src ="line.png" alt="left" width ="25" height="25"><p class="catalogtext">Catalog</p></button></div>
+                <button class="dropbtn"><img class="img" src ="image/line.png" alt="left" width ="25" height="25"><p class="catalogtext">Catalog</p></button></div>
             <div class="dropdown-content">
-                <a href="b2.php"><img src ="cupi.png" alt ="cupi" width ="15px" height ="15px">Flour products</a>
-                <a href="meal.php"><img src ="meal.png" alt ="meal" width ="15px" height ="15px">Meat</a>
-                <a href="SeaFood.php"><img src ="fish.png" alt ="fish" width ="15px" height ="15px">SeaFood</a>
-                <a href="Fruits.php"><img src ="fruit.png" alt ="fruit" width ="15px" height ="15px">Fruits</a>
-                <a href="Vegetables.php"><img src ="vegan.png" alt ="Vegetables" width ="15px" height ="15px">Vegetables</a>
-                <a href="Drinks.php"><img src ="drink.png" alt ="drinks" width ="15px" height ="15px">Drinks</a>
+                <a href="b2.php"><img src ="image/cupi.png" alt ="cupi" width ="15px" height ="15px">Flour products</a>
+                <a href="meal.php"><img src ="image/meal.png" alt ="meal" width ="15px" height ="15px">Meat</a>
+                <a href="SeaFood.php"><img src ="image/fish.png" alt ="fish" width ="15px" height ="15px">SeaFood</a>
+                <a href="Fruits.php"><img src ="image/fruit.png" alt ="fruit" width ="15px" height ="15px">Fruits</a>
+                <a href="Vegetables.php"><img src ="image/vegan.png" alt ="Vegetables" width ="15px" height ="15px">Vegetables</a>
+                <a href="Drinks.php"><img src ="image/drink.png" alt ="drinks" width ="15px" height ="15px">Drinks</a>
             </div>
         </div>
         <div class="dropdown">
             <div id = "list">
-                <button class="contacts" onclick="carsFunction()"><p class="contacttext"><img src="phone.png" width="25" height="25" class="contactimg"> Contacts</p></button></div>
+                <button class="contacts" onclick="carsFunction()"><p class="contacttext"><img src="image/phone.png" width="25" height="25" class="contactimg"> Contacts</p></button></div>
             <div class="dropdown-content">
                 <a href="#"><p>Email:onlineshop@mail.ru</p></a>
                 <a href="#"><p>Phone number:+7(777)-777-77-77</p></a>
@@ -79,7 +79,7 @@
             <input class="search" type = "text" style ="float:right;" name="search" placeholder="Search">
             <button  type = "submit" class="sub" name = "submit" >&#128270;</button>
         </form>
-        <div><a href="basket.html" title="Basket"><img src="basket.png" class="basimg"></a></div>
+        <div><a href="basket.php" title="Basket"><img src="image/basket.png" class="basimg"></a></div>
     </div>
 </header>
 <hr style="color: black;">
