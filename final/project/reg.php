7cd15a865046f62904df8fab3347cf21236aa927
nurg1ssa <57891947+nurg1ssa@users.noreply.github.com>
2020-06-15 21:48:29 +0600

    Add files via upload

---
+++ final/project/reg.php
@@ -0,0 +1,26 @@
+<?php
+require_once('config.php');
+?>
+<?php
+
+if(isset($_POST)) {
+
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $telnum = $_POST['telnum'];
+    $password = sha1($_POST['password']);
+
+        $sql = "INSERT INTO registration (full_name,email,telnum,password) VALUES(?,?,?,?)";
+        $stmtinsert = $db->prepare($sql);
+
+        $result = $stmtinsert->execute([$full_name, $email, $telnum, $password]);
+    if ($result) {
+        echo 'Successfully saved';
+    } else {
+        echo 'There were errors while saving the data.';
+    }
+}
+else{
+    echo 'No data';
+}
+?>
