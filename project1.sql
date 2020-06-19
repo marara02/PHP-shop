261104a6119b3eb53d2b027069204953704dde5a
marara02 <57908460+marara02@users.noreply.github.com>
2020-06-19 19:01:36 +0600

    Add files via upload

--- project1.sql
+++ project1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Июн 13 2020 г., 09:41
+-- Время создания: Июн 19 2020 г., 09:15
 -- Версия сервера: 8.0.18
 -- Версия PHP: 7.3.16
 
@@ -31,7 +31,7 @@ CREATE TABLE `cart` (
   `cart_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -42,7 +42,7 @@ CREATE TABLE `cart` (
 CREATE TABLE `catalog` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `catalog`
@@ -59,6 +59,21 @@ INSERT INTO `catalog` (`category_id`, `category_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `Full_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telnum` varchar(10) NOT NULL,
+  `address` varchar(90) NOT NULL,
+  `Total_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `product`
 --
 
@@ -69,7 +84,7 @@ CREATE TABLE `product` (
   `amount` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `img` varchar(280) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `product`
@@ -77,8 +92,8 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`product_id`, `product_name`, `price`, `amount`, `category_id`, `img`) VALUES
 (1, 'Red Velvet', 2000, 10, 1, 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2004/1/23/1/ss1d26_red_velvet_cake.jpg.rend.hgtvcom.616.462.suffix/1371584132020.jpeg'),
-(2, 'Voopie', 3000, 10, 1, 'https://www.kingarthurflour.com/sites/default/files/recipe_legacy/1058-3-large.jpg'),
-(3, 'Japanese Cake', 1500, 10, 1, 'https://i.ytimg.com/vi/v9nI6pdw-HQ/maxresdefault.jpg'),
+(2, 'Voopie', 3000, 10, 1, 'https://img-global.cpcdn.com/recipes/8b9897d93350ccfe/751x532cq70/shokoladnyi-tort-vupi-pai-ili-bashnia-svobodnogho-padieniia-%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D0%BE%D0%B5-%D1%84%D0%BE%D1%82%D0%BE-%D1%80%D0%B5%D1%86%D0%B5%D0%BF%D1%82%D0%B0.jpg'),
+(3, 'Japanese Cake', 1500, 10, 1, 'https://chef.tm/public/pics/288/288521_0.jpg'),
 (4, 'Cookies', 500, 10, 1, 'image/cook.jpg'),
 (5, 'Cupcakes', 150, 10, 1, 'image/cap.jpg'),
 (6, 'Bread ', 100, 10, 1, 'https://www.biggerbolderbaking.com/wp-content/uploads/2020/04/Hearty-Yeast-Free-Bread-WS-Thumbnail.jpg'),
@@ -100,20 +115,20 @@ INSERT INTO `product` (`product_id`, `product_name`, `price`, `amount`, `categor
 (22, 'Strawberries', 3500, 12, 4, 'https://i5.walmartimages.ca/images/Enlarge/094/582/6000200094582.jpg'),
 (23, 'Pears', 2000, 20, 4, 'https://i5.walmartimages.ca/images/Enlarge/-40/242/999999-40242.jpg'),
 (24, 'Apple', 1800, 25, 4, 'https://5.imimg.com/data5/LM/DU/MY-22954806/apple-fruit-500x500.jpg'),
-(25, 'Grapefruit', 2300, 5, 4, 'https://i5.walmartimages.ca/images/Enlarge/efr/uit/Grapefruit.jpg'),
+(25, 'Grapefruit', 2300, 5, 4, 'https://befreshcorp.net/wp-content/uploads/2017/07/product-packshot-Grapefruit.jpg'),
 (26, 'Plum', 2500, 23, 4, 'https://i5.walmartimages.ca/images/Enlarge/_40/426/40426.jpg'),
 (27, 'Tomato', 2500, 30, 5, 'https://i5.walmartimages.ca/images/Enlarge/094/511/6000200094511.jpg'),
-(28, 'Carrot', 1200, 10, 5, 'https://i5.walmartimages.ca/images/Enlarge/271/747/6000191271747.jpg'),
-(29, 'Onion', 1200, 10, 5, 'https://i5.walmartimages.ca/images/Large/688/102/6000197688102.jpg'),
+(28, 'Carrot', 1200, 10, 5, 'https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-1200.jpg'),
+(29, 'Onion', 1200, 10, 5, 'https://cdn.shopify.com/s/files/1/1537/5553/products/00613_15abd93a-e239-45df-acdb-8485b40d546a_grande.jpg?v=1486440965'),
 (30, 'Sweet potato', 1000, 30, 5, 'https://i5.walmartimages.ca/images/Enlarge/-40/747/999999-40747.jpg'),
 (31, 'Cabbage', 2400, 40, 5, 'https://i5.walmartimages.ca/images/Enlarge/272/109/6000191272109.jpg'),
 (32, 'Asparagus', 3500, 50, 5, 'https://i5.walmartimages.ca/images/Enlarge/094/414/6000200094414.jpg'),
 (33, 'Nestea Lemon', 900, 100, 6, 'https://i5.walmartimages.ca/images/Enlarge/762/243/762243.jpg'),
-(34, 'Green tea', 5000, 5, 6, 'https://i5.walmartimages.ca/images/Large/681/561/6000199681561.jpg'),
-(35, 'Hot Chocolate', 4500, 7, 6, 'https://i5.walmartimages.ca/images/Enlarge/580/621/6000201580621.jpg'),
+(34, 'Green tea', 5000, 5, 6, 'https://images-na.ssl-images-amazon.com/images/I/91JMy2ISDaL._SL1500_.jpg'),
+(35, 'Hot Chocolate', 4500, 7, 6, 'https://images-na.ssl-images-amazon.com/images/I/81ZQ2GHB7%2BL._SX425_.jpg'),
 (36, 'Coffee', 5000, 30, 6, 'https://i5.walmartimages.ca/images/Enlarge/078/971/6000201078971.jpg\r\n'),
-(37, 'Water', 3500, 20, 6, 'https://i5.walmartimages.ca/images/Enlarge/904/949/6000200904949.jpg'),
-(38, 'Shake', 5000, 10, 6, 'https://i5.walmartimages.ca/images/Large/400/010/999999-8346400010.jpg');
+(37, 'Water', 3500, 20, 6, 'https://netstorage-nur.akamaized.net/images/pogudx28tfo00q7lp.jpg?imwidth=900'),
+(38, 'Shake', 5000, 10, 6, 'https://cache.sporter.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/a/satled-caramel-shake.jpg');
 
 -- --------------------------------------------------------
 
@@ -127,7 +142,7 @@ CREATE TABLE `registration` (
   `email` varchar(30) NOT NULL,
   `telnum` int(11) NOT NULL,
   `password` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `registration`
@@ -153,6 +168,12 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `catalog`
   ADD PRIMARY KEY (`category_id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
 
 --
 -- Индексы таблицы `product`
@@ -182,6 +203,12 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `catalog`
   MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `product`
