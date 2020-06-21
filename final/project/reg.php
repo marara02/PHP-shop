aaef03c8dc86c477d5ebfdf3eeec4cdeb4a452ca
nurg1ssa <57891947+nurg1ssa@users.noreply.github.com>
2020-06-21 23:36:24 +0600

    Update reg.php

--- final/project/reg.php
+++ final/project/reg.php
@@ -1,26 +1,24 @@
 <?php
-require_once('config.php');
+require_once('database/config.php');
 ?>
 <?php
-
 if(isset($_POST)) {
-
-    $full_name = $_POST['full_name'];
-    $email = $_POST['email'];
-    $telnum = $_POST['telnum'];
-    $password = sha1($_POST['password']);
-
-        $sql = "INSERT INTO registration (full_name,email,telnum,password) VALUES(?,?,?,?)";
-        $stmtinsert = $db->prepare($sql);
-
-        $result = $stmtinsert->execute([$full_name, $email, $telnum, $password]);
+    $u_fullname = $_POST['u_fullname'];
+    $u_email = $_POST['u_email'];
+    $u_address = $_POST['u_address'];
+    $u_telnum = $_POST['u_telnum'];
+    $u_card_n = $_POST['u_card_n'];
+    $u_card_e = $_POST['u_card_e'];
+    $u_card_c = $_POST['u_card_c'];
+    $sql="INSERT INTO user (`u_fullname`,`u_email`,`u_address`,`u_telnum`,`u_card_n`,`u_card_e`,`u_card_c`) VALUES(?,?,?,?,?,?,?)";
+    $stmtinsert = $db->prepare($sql);
+    $result = $stmtinsert->execute([$u_fullname, $u_email, $u_address, $u_telnum,$u_card_n,$u_card_e,$u_card_c]);
     if ($result) {
-        echo 'Successfully saved';
+        echo 'Your order will come as soon as possible';
     } else {
-        echo 'There were errors while saving the data.';
+        echo 'Something went wrong;(';
     }
 }
 else{
     echo 'No data';
 }
-?>
