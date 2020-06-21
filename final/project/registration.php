4d322ebd6ab5e7f72d38d36d410ed3597cefef20
nurg1ssa <57891947+nurg1ssa@users.noreply.github.com>
2020-06-21 23:35:23 +0600

    Update registration.php

--- final/project/registration.php
+++ final/project/registration.php
@@ -1,72 +1,36 @@
 <?php
-require_once('config.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<head>
-    <title>onlineshop.kz</title>
-    <!--<link rel="stylesheet" type="text/css" href="CSS.css">-->
-    <link rel="stylesheet" type="text/css" href="main.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:600,900" rel="stylesheet">
-    <link rel="shortcut icon" href="/shop.png" type="image/png">
-    <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Six+Caps&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Anton&display=swap" rel="stylesheet">
-    <link href  ="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Orbitron&display=swap" rel="stylesheet">
-    <link rel = "icon" href = "shop.png" type = "image/x-icon">
-    <link rel="stylesheet" type="text/css" href="header.css">
-    <script type="text/javascript" src="functions.js"></script>
-    <link rel="stylesheet" type="text/css" href="registration.css">
 
-</head>
-<header>
-    <div class="topnav">
-        <button class="home" onclick="homeFunction()"><img src="shop.png" class="animation" align="left" width="25"><p style="font-size:185%"> Home</p></button>
-        <div class="dropdown">
-            <div id = "list">
-                <button class="dropbtn"><img class="img" src ="line.png" alt="left" width ="25" height="25"><p class="catalogtext">Catalog</p></button></div>
-            <div class="dropdown-content">
-                <a href="b2.php"><img src ="cupi.png" alt ="cupi" width ="15px" height ="15px">Flour products</a>
-                <a href="meal.php"><img src ="meal.png" alt ="meal" width ="15px" height ="15px">Meals</a>
-                <a href="SeaFood.php"><img src ="fish.png" alt ="fish" width ="15px" height ="15px">SeaFood</a>
-                <a href="Fruits.php"><img src ="fruit.png" alt ="fruit" width ="15px" height ="15px">Fruits</a>
-                <a href="Vegetables.php"><img src ="vegan.png" alt ="Vegetables" width ="15px" height ="15px">Vegetables</a>
-                <a href="#"><img src ="drink.png" alt ="drinks" width ="15px" height ="15px">Drinks</a>
-            </div>
-        </div>
-        <div class="dropdown">
-            <div id = "list">
-                <button class="contacts" onclick="carsFunction()"><p class="contacttext"><img src="phone.png" width="25" height="25" class="contactimg"> Contacts</p></button></div>
-            <div class="dropdown-content">
-                <a href="#"><p>Email:onlineshop@mail.ru</p></a>
-                <a href="#"><p>Phone number:+7(777)-777-77-77</p></a>
-            </div>
-        </div>
-        <button class="log"><a href="Login.php" class="logtext">LogIn</a><p class="sign">/</p><a href="registration.php" class="regtext">Register</a></button>
-        <form method="post">
-            <input class="search" type = "text" style ="float:right;" name="search" placeholder="Search">
-            <button  type = "submit" class="sub" name = "submit" >&#128270;</button>
-        </form>
-        <div><a href="basket.html" title="Basket"><img src="basket.png" class="basimg"></a></div>
-    </div>
-</header>
-<div>
+require_once('database/authorization.php');
+include_once ('header.php')
+?>
+
+
+<div xmlns="http://www.w3.org/1999/html">
+    <link rel="stylesheet" type="text/css" href="CSS/registration.css">
+
+
 
 </div>
+
 <body>
-<hr style="color: black;">
+
+
 <div class="main">
-    <form class="form" method="post" action="registration.php">
+
+    <form class="form">
+
         <div class="f">
-            <h1>Registration</h1>
-            <input class="input" type="name" id="full_name" name="full_name" placeholder="Full Name" required>
-            <input class="input"  type="Email" id="email" name="email" placeholder="Email" required>
-            <input class="input" type="tel" name="telnum" id="telnum" placeholder="8(XXX)-XXX-XX-XX" minlength="11" maxlength="11" required>
-            <input class="input" type="password" name="password" placeholder="Password" id="password" minlength="8" required>
-            <input class="input" type="password" name="pass2" placeholder="Resset Password" id="pass2" minlength="8" required>
+
+            <h1>User information form</h1>
+
+            <input class="input" type="text" id="u_fullname" name="u_fullname" placeholder="Full Name" required>
+            <input class="input"  type="Email" id="u_email" name="u_email" placeholder="Email" required>
+            <input class="input" type="tel" name="u_telnum" id="u_telnum" placeholder="8(XXX)-XXX-XX-XX" minlength="11" maxlength="11" required>
+            <input class="input" type="text" name="u_address" id="u_address" placeholder="Address" required>
+            <h3>Card information</h3>
+            <input class="input" type="tel" name="u_card_n" id="u_card_n" minlength="16" maxlength="16" placeholder="Card number" required>
+            <input class="input" type="text" name="u_card_e" id="u_card_e" minlength="5" maxlength="5" placeholder="MM/YY"required>
+            <input class="input" type="tel" name="u_card_c" id="u_card_c" minlength="3" maxlength="3" placeholder="CCV" required>
             <input class="inputsub" type="submit" name="register" id="register" value="Submit">
         </div>
     </form>
@@ -78,15 +42,18 @@ require_once('config.php');
         $('#register').click(function(e) {
             var valid = this.form.checkValidity();
             if(valid) {
-                var full_name = $('#full_name').val();
-                var email = $('#email').val();
-                var telnum = $('#telnum').val();
-                var password = $('#password').val();
+                var u_fullname = $('#u_fullname').val();
+                var u_email = $('#u_email').val();
+                var u_telnum = $('#u_telnum').val();
+                var u_address = $('#u_address').val();
+                var u_card_n = $('#u_card_n').val();
+                var u_card_e = $('#u_card_e').val();
+                var u_card_c = $('#u_card_c').val();
                 e.preventDefault();
                 $.ajax({
                     type: 'POST',
                     url: 'reg.php',
-                    data:{full_name:full_name, email:email, telnum:telnum, password:password,},
+                    data:{u_fullname:u_fullname,u_email:u_email, u_telnum:u_telnum, u_address:u_address,u_card_n:u_card_n,u_card_e:u_card_e,u_card_c:u_card_c},
                     success: function (data) {
                         Swal.fire(
                             'Congratulations!',
@@ -105,12 +72,12 @@ require_once('config.php');
             }else{
             }
         });
-
     });
 </script>
-<footer class="fot" style="">
-    <br>
-    <p>follow us on instagram:@onlineshop</p>
-</footer>
+<?php
+include_once 'footer.php';
+?>
+
 </body>
+
 </html>
