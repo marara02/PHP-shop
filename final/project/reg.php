a1f1300cfa0402e218d99eadfa7b6b9d5de41fab
nurg1ssa <57891947+nurg1ssa@users.noreply.github.com>
2020-06-21 23:29:38 +0600

    Add files via upload

--- final/project/reg.php
+++ final/project/reg.php
@@ -1,30 +1,23 @@
 <?php
-include_once('database/authorization.php');
-
+require_once('config.php');
+?>
+<?php
 
 if(isset($_POST)) {
+
     $full_name = $_POST['full_name'];
     $email = $_POST['email'];
-    $address = $_POST['address'];
-    $tel_num = '10000';
-    $cost = $_POST['cost'];
-    try{
-    $sql = "INSERT INTO `orders`( `Full_name`, `email`, `address`, `telnum` ,`Total_price`) VALUES (?,?,?,?,?)";
-    $stmt = $link->prepare($sql);
-    $stmt->bind_param("ssssi",$full_name,$email,$address,$tel_num,$cost);
-    $result = $stmt->execute();
-    $stmt->close();
+    $telnum = $_POST['telnum'];
+    $password = sha1($_POST['password']);
+
+        $sql = "INSERT INTO registration (full_name,email,telnum,password) VALUES(?,?,?,?)";
+        $stmtinsert = $db->prepare($sql);
+
+        $result = $stmtinsert->execute([$full_name, $email, $telnum, $password]);
     if ($result) {
         echo 'Successfully saved';
     } else {
-        throw new Exception("Something went wrong");
-    }
-}
-catch (Exception $exception){
-        print "An Exception occurred. Message:" .$exception->getMessage();
-    }
-    catch (Error $error){
-        print "An error occurred. Message:".$error->getMessage();
+        echo 'There were errors while saving the data.';
     }
 }
 else{
